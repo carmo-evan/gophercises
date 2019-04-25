6c99f03f758aa222f074b9dcdec066ee002ff73f
Evan Do Carmo <evan.docarmo@ten24web.com>
2019-04-25 07:08:46 -0400

    refactor

--- link/link/link.go
+++ link/link/link.go
@@ -12,13 +12,40 @@ type Link struct {
 	Text string
 }
 
-// join concatenates strings
-func join(strs ...string) string {
-	var sb strings.Builder
-	for _, str := range strs {
-		sb.WriteString(str)
+//standardizeSpaces substitutes multiple spaces for just one
+func standardizeSpaces(s string) string {
+	ss := strings.Fields(s)
+	return strings.Join(ss, " ")
+}
+
+//assignTextToLink appends the input text to the existing text in the Link
+func assignTextToLink(bytes []byte, link *Link) {
+	text := standardizeSpaces(string(bytes))
+	if len(text) > 0 && len(link.Text) > 0 {
+		stringsToJoin := []string{link.Text, text}
+		link.Text = strings.Join(stringsToJoin, " ")
+	} else if len(text) > 0 {
+		link.Text = text
 	}
-	return sb.String()
+}
+
+func isAnchorLink(tagName []byte) bool {
+	return len(tagName) == 1 && tagName[0] == 'a'
+}
+
+func assignHrefToLink(link *Link, tokenizer *html.Tokenizer) {
+	for {
+		key, val, moreAttr := tokenizer.TagAttr()
+		attrName := string(key)
+		attrValue := string(val)
+		if attrName == "href" {
+			link.Href = attrValue
+			break
+		}
+		if !moreAttr {
+			break
+		}
+	}
 }
 
 //Parse takes a pointer to an HTML file and parses all anchor tags into an array of the Link type
@@ -28,8 +55,8 @@ func Parse(r io.Reader) []Link {
 	var currentLink Link
 	var links []Link
 	for {
-		tt := tokenizer.Next()
-		switch tt {
+		tokenType := tokenizer.Next()
+		switch tokenType {
 		case html.ErrorToken:
 			err := tokenizer.Err()
 			if err == io.EOF {
@@ -37,28 +64,21 @@ func Parse(r io.Reader) []Link {
 			}
 		case html.TextToken:
 			if depth > 0 {
-				text := string(tokenizer.Text())
-				currentLink.Text = join(currentLink.Text, text)
+				assignTextToLink(tokenizer.Text(), &currentLink)
 			}
-		case html.StartTagToken, html.EndTagToken:
+		case html.StartTagToken:
+			tagName, _ := tokenizer.TagName()
+			if isAnchorLink(tagName) {
+				var newLink Link
+				currentLink = newLink
+				assignHrefToLink(&currentLink, tokenizer)
+				depth++
+			}
+		case html.EndTagToken:
 			tn, _ := tokenizer.TagName()
-			if len(tn) == 1 && tn[0] == 'a' {
-				if tt == html.StartTagToken {
-					var newLink Link
-					currentLink = newLink
-					key, val, _ := tokenizer.TagAttr()
-					attrName := string(key)
-					attrValue := string(val)
-					if attrName == "href" {
-						currentLink.Href = attrValue
-					}
-					depth++
-				} else {
-					if len(tn) == 1 && tn[0] == 'a' {
-						links = append(links, currentLink)
-					}
-					depth--
-				}
+			if isAnchorLink(tn) {
+				links = append(links, currentLink)
+				depth--
 			}
 		}
 	}
