079911379f8811d9253aa72dc49553be32a3d221
Evan Do Carmo <evan.docarmo@ten24web.com>
2019-07-16 10:09:20 -0400

    using workers pattern

--- quiet_hn/main.go
+++ quiet_hn/main.go
@@ -34,47 +34,54 @@ func main() {
 func handler(client hn.Client, numStories int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		ids, err := client.TopItems() // returns around 450 unique ids
+		ids, err := client.TopItems() // returns around 500 unique ids
 		if err != nil {
 			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
 			return
 		}
-		s := newStories() //holds map of items, slice of ids, mutex
-		wg := &sync.WaitGroup{}
-		for _, id := range ids {
-			wg.Add(1)
-			go func(id int) {
+		tasks := make(chan int, len(ids))
+		results := make(chan item, len(ids))
+		workersCount := 30
+		wg := sync.WaitGroup{}
+		wg.Add(workersCount)
+		for i := 0; i < workersCount; i++ {
+			go func() {
 				defer wg.Done()
-				hnItem, err := client.GetItem(id) // reach out to API to get full item details
-				if err != nil {
-					return
+				for id := range tasks {
+					hnItem, err := client.GetItem(id) // reach out to API to get full item details
+					if err != nil {
+						panic(err)
+					}
+					item := parseHNItem(hnItem)
+					results <- item
 				}
-				item := parseHNItem(hnItem)
-				if isStoryLink(item) { // if it's the type we want, let's save it
-					s.mu.Lock()
-					defer s.mu.Unlock()
-					s.items[id] = item        // save item
-					s.ids = append(s.ids, id) // save id
-					//issue here is, we are still reaching out to the API 450 times
-					// Yes, we do it concurrently, but that's a lot of times.
-					// Benchmark ends up being the same as hitting it 30 times sequentially.
-					// so concurrency right now does not represent any performance gain
-				}
-			}(id)
-
+			}()
 		}
+
+		for _, id := range ids {
+			tasks <- id
+		}
+
+		close(tasks)
+
 		wg.Wait()
+
 		var items []item
-		sort.Ints(s.ids)
-		for _, id := range s.ids {
-			item, _ := s.items[id]
-			items = append(items, item)
-			if len(items) == numStories { //this is where we are capping the displayed items to 30
-				break
+
+		for item := range results {
+			if isStoryLink(item) {
+				items = append(items, item)
 			}
 		}
+
+		close(results)
+
+		sort.Slice(items, func(i, j int) bool {
+			return items[i].ID < items[j].ID
+		})
+
 		data := templateData{
-			Stories: items,
+			Stories: items[:30],
 			Time:    time.Now().Sub(start),
 		}
 		err = tpl.Execute(w, data)
